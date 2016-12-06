aec9dab2105f6272764041e10b78b5346436b918
= <phamquan1105@gmail.com>
2016-12-06 09:33:59 +0700

    Adding new project with client implement with API

---
+++ src/main/java/com/vccorp/useragent/client/UAParser.java
@@ -0,0 +1,82 @@
+package com.vccorp.useragent.client;
+
+import com.vccorp.useragent.config.SystemInfo;
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+
+/**
+ * User Agent Parser Client
+ * 
+ * @author thanhnd
+ * 
+ */
+public class UAParser {
+
+	private static UserAgentParser.Client uaParser;
+	private static TTransport transport;
+	private static UAParser instance = null;
+
+
+	/**
+	 * Constructor for class parser user agent string
+	 */
+	public UAParser() {
+		try {
+			String host= SystemInfo.THRIFT_SERVER_HOST;
+			System.err.println(host);
+			transport = new TSocket(host, SystemInfo.THRIFT_SERVER_PORT);
+
+			transport.open();
+			TProtocol protocol = new TBinaryProtocol(transport);
+			uaParser = new UserAgentParser.Client(protocol);
+		} catch (TException x) {
+			x.printStackTrace();
+		}
+	}
+
+	/**
+	 * create single instance
+	 * @return
+	 */
+	public static UAParser getInstance () {
+		if (instance==null) {
+			instance = new UAParser();
+		}
+		return instance;
+	}
+
+	/**
+	 *
+	 * @param userAgentString raw string user agent</br>
+	 * @return type of os ; os version; model name of device
+	 */
+	public String parse(String userAgentString) {
+		try {
+			String info = uaParser.parse(userAgentString);
+			String[] splits = info.split(";");
+			String value = splits[0] + ";" + splits[1] + ";" + splits[3] + ";"
+					+ splits[4];
+			return value;
+		}catch (TException e) {
+			return null;
+		}
+	}
+
+
+	/**
+	 * close transport
+	 */
+	public static void close() {
+		transport.close();
+	}
+	
+	public static void main(String[] args) {
+		UAParser ua = UAParser.getInstance();
+			String userAgentString =
+					"Mozilla/5.0 (iPhone; CPU iPhone OS 7_1_2 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) CriOS/46.0.2490.85 Mobile/11D257 Safari/9537.53";
+			System.out.println(ua.parse(userAgentString));
+	}
+}
---
+++ src/main/java/com/vccorp/useragent/config/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by quanpv on 06/12/2016.
+ * this package contain all common task that process configuration
+ */
+package com.vccorp.useragent.config;
